16e5a6bfdd7c93bde3d1448da1bf6d22d8300006
Anny <netaklerow@gmail.com>
2017-04-18 16:19:35 +0300

    Add files via upload

---
+++ Init.hs
@@ -0,0 +1,55 @@
+module Init where
+
+import System.Random
+import Graphics.Gloss.Geometry.Line
+import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Interface.Pure.Simulate
+import Graphics.Gloss.Juicy
+import Types
+
+-- | Инициализировать игровую вселенную.
+initUniverse :: StdGen -> Universe
+initUniverse g = Universe
+  { universePlatforms  = absolutePlatforms (initPlatforms g)
+  , universePlayer = initPlayer
+  , universeScore  = 0
+  , universeBackground = initBackground
+  , universeGameOver = Nothing
+  }
+
+-- | Создать бесконечный список платформ.
+absolutePlatforms :: [Platform] -> [Platform]
+absolutePlatforms = go 0  
+  where
+    go  _ [] = []
+    go  s ((w, o, t) : gs) = (w, s - o, t) : (go (s - o) gs)
+
+-- | Инициализировать начальное состояние игрока.
+initPlayer :: Player
+initPlayer = Player
+  { playerHeight = 300
+  , playerWidth = 0
+  , playerSpeed = 0
+  , playerIsOnPlatform = False
+  , playerFallingSpeed  = 0
+  }
+
+-- | Инициализировать одну платформу.
+initPlatform :: Width -> Platform
+initPlatform h = (h, defaultOffset, timeOfLife)
+
+-- | Инициализировать случайный бесконечный
+-- список платформ для игровой вселенной.
+initPlatforms :: StdGen -> [Platform]
+initPlatforms g = map initPlatform
+  (randomRs platformWidthRange g)
+
+initBackground :: Background
+initBackground = Background
+  { bgHeight1 = bgHeight / 2
+  , bgHeight2 = -bgHeight / 2
+  , bgSpeed = 70
+  }
+-- | Инициализировать конец игры.
+initGameOver :: Point
+initGameOver = (0.32, 0.32)
